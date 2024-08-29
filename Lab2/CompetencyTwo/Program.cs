8ea3faa80ed3532907aa3e81378d381a9b54ad53
Corn <cornellburts@yahoo.com>
2024-08-29 19:22:28 -0400

    Added comments for the questions to be answered

--- Lab2/CompetencyTwo/Program.cs
+++ Lab2/CompetencyTwo/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+namespace LabOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /* Add your name, course number, and CRN as a multi-line comment at the beginning of your code.*/
+
+        /* Create 5 integer variables u, v, w, x, and y. Assign values to each of them. Output the sum: u + v + w + x + y. */
+
+        /* Create a variable called name. Assign your name to it. Create a variable called major. Assign your major to the variable called major. Use string interpolation ($) to Output: [Your name]'s major is [major]. Example: student named John Smith whose major is Programming will output John's major is programming.*/
+
+        /* Use the following given code Console.Write("Please enter the price: "); to prompt a user to enter a value in the console. Use the code string inputstr = Console.ReadLine( ); to read in a variable (whole numbers only) from the console. Then convert the variable inputstr to an integer value and assign it to an int variable called inputint. Then multiply inputint by 1.08 and output "The total price with tax is " + inputint (use the concatenation operator +). */
+
+        /* Use the increment operator to increment to add 1 to the number below and output the result. */
+
+        /* Create a string variable called season and assign your favorite season to it. Concatenate it to the following string using string interpolation($) and output the value. */
+
+        /* Output the following programming language variables separated by commas using the string concatenation operator(+). Your output will be C#, HTML, CSS, Python, Java */
+
+        /* Divide the following variable by 100 and output the value. */
+
+        /* Add the two values together and output the sum. */
+
+        /* Evaluate the expression d * m + (s * 3) + 4 * m using the given values for d, m, and s. Output the value. */
+
+        /* Convert the address to all uppercase and output the value using a single Console.WriteLine() statement and the newline escape sequence, so each variable is on a separate line */
+
+
+        }
+    }
+}
