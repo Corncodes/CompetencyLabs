505485b6877f2959b276aaf838bb450e42c56410
Corn <cornellburts@yahoo.com>
2024-09-10 17:02:21 -0400

    Finished 9, 8 is in the works

--- Lab4/CompetencyFour/Program.cs
+++ Lab4/CompetencyFour/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
@@ -43,7 +44,7 @@ namespace LabFour
 
             // 5. Use a for loop to output values 61 to 72 on one line with one space between the values. Increment by 1. Note: 61 and 72 are inclusive.
 
-            for (int b=61; b<=72; b++)
+            for (int b = 61; b <= 72; b++)
             {
                 Console.Write(b + " ");
             }
@@ -51,7 +52,7 @@ namespace LabFour
 
             // 6. Use a for loop to loop from 1-100. Each time through the loop, output the loop control variable and whether or not it is even or odd. Output on separate lines
 
-            for (int c = 1; c<=100; c++)
+            for (int c = 1;c <= 100; c++)
             {
                 if ((c % 2 == 0))
                 {
@@ -64,6 +65,7 @@ namespace LabFour
             }
 
             // 7. Use a while loop. Allow a user to input a value. Convert the input to an integer. If the input user enters equals 8, end the loop. Otherwise, keep asking for input.
+
             Console.WriteLine("Enter an even integer lower than 20");
             int y = Convert.ToInt32(Console.ReadLine());
             while (y != 8)
@@ -79,12 +81,28 @@ namespace LabFour
                     y = Convert.ToInt32(Console.ReadLine());
                 }
             }
-            Console.WriteLine("Proud of your resilience, you have guessed the correct number");
+            Console.WriteLine("Proud of your resilience, you have guessed the correct number!");
 
             // 8. Use a for loop to output the sum of odd integers from 1 to 50.
 
+            // for (int s = 0, sum = 0; s <= 50; s++)
+            // {
+            //     if (s % 2 == 1)
+            //     {
+            //         sum += s;
+            //         Console.Write("sum equals" + sum);
+            //     }
+            // }
+            // Console.WriteLine();
 
             // 9. Use a while loop to output the following – 5 rows with 10 asterisks on each line
+
+            int o = 1;
+            while (o <= 5)
+            {
+                Console.WriteLine("**********");
+                o++;
+            }
 
 
         }
