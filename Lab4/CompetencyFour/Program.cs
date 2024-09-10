65d86db9cf9c5270398df6baabec8423f67cb53b
Corn <cornellburts@yahoo.com>
2024-09-10 14:11:30 -0400

    Added questions to the file

--- Lab4/CompetencyFour/Program.cs
+++ Lab4/CompetencyFour/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LabThree
+{
+    class SecondProgram
+    {
+        static void Main(string[] args)
+        {
+            // 1. Add student name, course number, and CRN as a multi-line comment at the beginning of your code.
+            /* Cornell Burts, IT 1050, & 86411 */
+
+            // 2. In a comment, compare and contrast the if (single-selection) statement and while loop. How are the statements similar? How are they different?
+            
+            // 3. Use a while loop to output values 48 to 92 on one line with one space between the values. Increment by 4. Note: 48 and 92 are inclusive.
+
+            // 4. Use a while loop to output values 98 to 21 on one line with one space between the values. Decrement by 7. Note: 98 and 21 are inclusive.
+
+            // 5. Use a for loop to output values 61 to 72 on one line with one space between the values. Increment by 1. Note: 61 and 72 are inclusive.
+
+            // 6. Use a for loop to loop from 1-100. Each time through the loop, output the loop control variable and whether or not it is even or odd. Output on separate lines
+
+            // 7. Use a while loop. Allow a user to input a value. Convert the input to an integer. If the input user enters equals 8, end the loop. Otherwise, keep asking for input.
+
+            // 8. Use a for loop to output the sum of odd integers from 1 to 50.
+
+            // 9. Use a while loop to output the following – 5 rows with 10 asterisks on each line
+        }
+    }
+}
