cda0c01740f876ab586f05456aad860851a47cd8
Corn <cornellburts@yahoo.com>
2024-10-15 14:49:07 -0400

    Put in the questions to be answered

--- Lab6/CompetencySix/Program.cs
+++ Lab6/CompetencySix/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Add name, course number, and CRN as a multi-line comment at the beginning of the code.
+            /* Cornell Burts, IT 1050, 86411 */
+
+            // 2. Create a class named Student.
+
+            // 3. Add a property called name. Assign your name to the name property.
+
+            // 4. Add a method called getMajor(). Have it return the major you are currently pursuing at Tri-C
+
+            // 5. Add a method called getCourse(). Have it return our course number and course title as listed on the syllabus.
+
+            // 6. Add a method called printName. It should not return any values. In the body of the method, add the following line: Console.WriteLine(this.name);
+
+            // 7. Create an object called student1.
+
+            // 8. Output the value of the name property (student1.name) inside of a Console.WriteLine().
+
+            // 9. Output the return value from student1.getMajor()
+
+            // 10.Output the return value from student1.getCourse().
+
+            // 11.Call printName() to output name again.
+        }
+    }
+}
