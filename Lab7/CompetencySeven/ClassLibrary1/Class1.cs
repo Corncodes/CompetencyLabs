eafacc51fee90d7f98418ca857a380d3e569d337
Corn <cornellburts@yahoo.com>
2024-10-24 17:02:29 -0400

    Added Lab Questions

---
+++ Lab7/CompetencySeven/ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibrary1
+{
+    public class Book
+    {
+        public string author = "";
+        //private string isbn = "";
+        public double price ;
+        public string title = "";
+        public Book()
+    {
+    }
+    public Book(string title, string author){
+        this.author = author;
+        this.title = title;
+    }
+    public string getTitle()
+    {
+        return this.title;
+    }
+    public void setTitle(string title)
+    {
+        this.title = title;
+    }
+
+    }
+}
--- Lab7/CompetencySeven/Program.cs
+++ Lab7/CompetencySeven/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+// 1. Add name, the course number, and the course CRN as a comment at the beginning of the code. Use either single line or multi-line comment syntax.
+// 2. Add exception handling to make sure dividing by 0 does not crash the program.
+// 3. Change the value of num2 from 10 to 0 to show that your exception handling was done correctly. This line should output results of your exception handling.
+// 4. Add a constructor method that accepts 2 parameters: isbn and title. Assign the parameters values to the class properties.
+// 5. Add a constructor method that accepts 4 parameters: isbn, title, author, year. Assign the parameters values to the class properties.
+// 6. Add a constructor method that accepts 6 parameters (isbn, title, author, publisher, year, pages) and assigns values to all of the properties of the class. Assign the parameters values to the class properties.
+// 7. Uncomment the lines that start with Book book2 = new Book("978-0134601540", "Visual C# How to Program"); and end with book2.Display(); to display details for book2.
+// 8. Uncomment the lines that start with Book book3 = new Book("978-1337102100", "Microsoft Visual C#: An Introduction to Object-Oriented Programming ", "Joyce Farrell",  2017); and end with book3.Display(); to display details for book3.
+// 9. Uncomment the lines that start with Book book4 = new Book("978-0135181966", "The Object-Oriented Thought Process", "Matt Weisfeld", "Addison-Wesley", 2019, 240); and end with book4.Display(); to display details for book4.
